d2ed4a80ff765519be44604d978effdbbb85541b
Allen Guo <guosonglin001@gmail.com>
2023-03-02 13:10:30 +0000

    update llvm downloader

--- t.sh
+++ t.sh
@@ -1,9 +1,9 @@
 # dev in docker image:
 # https://github.com/gglin001/Dockerfiles/blob/master/clangdev/Dockerfile
 
-git clone https://github.com/llvm/llvm-project.git --single-branch -b main --depth=1000
+git clone https://github.com/llvm/llvm-project.git --single-branch -b main --depth=1
 
-pushd llvm-project && git checkout $(cat ../build_tools/llvm_version.txt) && popd
+pushd llvm-project && git fetch --depth 1 origin $(cat ../build_tools/llvm_version.txt) && popd
 
 bash build_tools/build_mlir.sh ${PWD}/llvm-project/ ${PWD}/llvm-build
 
