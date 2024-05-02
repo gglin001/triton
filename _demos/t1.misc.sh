367806cabc99978ec2aaf400695bf68ff263d19e
Allen Guo <guosonglin001@gmail.com>
2024-05-02 16:18:02 +0800

    wip

--- _demos/t1.misc.sh
+++ _demos/t1.misc.sh
@@ -2,4 +2,7 @@ triton-opt --help >_demos/triton-opt.help.log
 triton-llvm-opt --help >_demos/triton-llvm-opt.help.log
 triton-shared-opt --help >_demos/triton-shared-opt.help.log
 
+lit --help >_demos/lit.help.log
+FileCheck --help >_demos/FileCheck.help.log
+
 ###############################################################################
