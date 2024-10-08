e7bb68ba673c84424b974eeb271780c3fe61e081
Allen Guo <guosonglin001@gmail.com>
2024-10-08 22:20:07 +0800

    wip

--- _demos/t1.misc.sh
+++ _demos/t1.misc.sh
@@ -1,11 +1,14 @@
 ###############################################################################
 
-triton-opt --help >_demos/triton-opt.help.log
-triton-llvm-opt --help >_demos/triton-llvm-opt.help.log
-triton-shared-opt --help >_demos/triton-shared-opt.help.log
+mkdir -p _demos/_log
 
-lit --help >_demos/lit.help.log
-FileCheck --help >_demos/FileCheck.help.log
+triton-opt --help >_demos/_log/triton-opt.help.log
+triton-llvm-opt --help >_demos/_log/triton-llvm-opt.help.log
+triton-reduce --help >_demos/_log/triton-reduce.help.log
+# triton-shared-opt --help >_demos/_log/triton-shared-opt.help.log
+
+lit --help >_demos/_log/lit.help.log
+FileCheck --help >_demos/_log/FileCheck.help.log
 FileCheck --dump-input=help
 
 ###############################################################################
